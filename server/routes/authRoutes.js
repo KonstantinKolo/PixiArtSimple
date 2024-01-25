6c0255f102ed25514ffee2c99d44af47972ea0cd
Konstantin <konstantinkolovski_zh21@schoolmath.eu>
2024-01-26 00:23:19 +0200

    backend fixes

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -7,7 +7,7 @@ const { test, registerUser, loginUser, getProfile, updatePicCollection} = requir
 router.use(
   cors({
     credentials: true,
-    origin: 'http://localhost:5173'
+    origin: 'pixiartsimple.netlify.app'
   })
 )
 
