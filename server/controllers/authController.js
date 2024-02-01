0261b2a51f01f287a689289d15b8dd220e054a46
Konstantin <konstantinkolovski_zh21@schoolmath.eu>
2024-02-01 08:45:06 +0200

    fix47

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -67,14 +67,16 @@ const loginUser = async (req, res) => {
     const match = await comparePasswords(password, user.password)
     console.log(3);
     if(match) {
-      // jwt.sign({email: user.email, id: user._id, name: user.name, picCollection: user.picCollection, profilePicture: user.profilePicture}, process.env.JWT_SECRET, {}, (err, token) => {
-      //   if(err) throw err;
-      //   res.cookie('token', token, {
-      //     httpOnly: true,
-      //     sameSite: 'none', 
-      //     secure: true, // set to true if using HTTPS
-      //   }).json(user);
-      // })
+      jwt.sign({email: user.email, id: user._id, name: user.name, picCollection: user.picCollection, profilePicture: user.profilePicture}, process.env.JWT_SECRET, {}, (err, token) => {
+        if(err) throw err;
+        res.cookie('token', token 
+        // ,{
+          // httpOnly: true,
+          // sameSite: 'none', 
+          // secure: true, // set to true if using HTTPS
+        // }
+        ).json(user);
+      })
       console.log('hey');
     }
     if(!match){
